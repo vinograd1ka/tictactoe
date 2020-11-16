9eadc2f4aff66b302108f7e34ebb4d59fb1e1b45
vinograd1ka <kachanov220@mail.ua>
2020-11-16 13:27:56 +0200

    made version tictactoe on else if logic

--- script.js
+++ script.js
@@ -5,22 +5,31 @@
     squaresEl.forEach(function (entry) {
         entry.addEventListener("click", function (event){
             var div = document.createElement("div");
-            var display = 'block';
             div.className = "container"
-
-            if (inputEl.value === 'square'){
-                div.innerHTML = '<div class="add-square" id="add-square"></div>';
+            if(entry.innerHTML === '<div class="container"><img src="smile.jpg" alt=""></div>'){
+                entry.innerHTML = '';
+            }
+            else if(entry.innerHTML === '<div class="container"><div class="add-square"></div></div>'){
+                entry.innerHTML = '';
+            }
+            else if(entry.innerHTML === '<div class="container"><div class="add-circle"></div></div>'){
+                entry.innerHTML = '';
+            } else if(entry.innerHTML === '<div class="container"><div class="add-cross"></div></div>'){
+                entry.innerHTML = '';
+            } else if (inputEl.value === 'square'){
+                div.innerHTML = '<div class="add-square"></div>';
+                entry.appendChild(div);
             } else if (inputEl.value === 'cross'){
                 div.innerHTML = '<div class="add-cross"></div>';
+                entry.appendChild(div);
             } else if (inputEl.value === 'circle'){
                 div.innerHTML = '<div class="add-circle"></div>';
-            } else{
-                div.innerHTML = '<img src="smile.jpg" alt="">';
-                display='none';
+                entry.appendChild(div);
             }
-
-            entry.appendChild(div);
-            document.getElementsByClassName('add-square').style.display=display;
+            else{
+                div.innerHTML = '<img src="smile.jpg" alt="">';
+                entry.appendChild(div);
+            }
         });
     });
 })();
